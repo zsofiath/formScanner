0962c284ca4b670edd5c706c06bc26cfeb29a557
Tóth Zsófia <zsofiath@gmail.com>
2020-09-28 12:07:06 +0200

    typing and empty field

--- scanner.js
+++ scanner.js
@@ -1,4 +1,5 @@
 var usagePackage = {};
+var eventNameList = ["click", "blur", "change", "keyup"];
 
 document.addEventListener("DOMContentLoaded", function(){
     input =  Array.prototype.slice.call(document.getElementsByTagName("input"), 0);
@@ -6,30 +7,32 @@ document.addEventListener("DOMContentLoaded", function(){
     textarea =  Array.prototype.slice.call(document.getElementsByTagName("textarea"), 0);
     button =  Array.prototype.slice.call(document.getElementsByTagName("button"), 0);
 
-   var elments = [].concat(input).concat(select).concat(textarea).concat(button)
-
-
-   var eventNameList = ["click", "blur", "change", "keyup"];
+   var elements = [].concat(input).concat(select).concat(textarea).concat(button)
    
-    elments.forEach(htmlElement => {
+   eddEventListenersToEveryFormElement(elements);
+});
+
+function eddEventListenersToEveryFormElement(elements){
+    elements.forEach(htmlElement => {
         eventNameList.forEach(function(eventName){
             setListeners(eventName, htmlElement)
         })
     });
-});
+}
 
 function setListeners(eventName, htmlElement){
-    htmlElement.addEventListener(eventName, function(event){
+    htmlElement.addEventListener(eventName, function(event) {
+        addExtraOperationsForListeners(eventName, event, htmlElement);
         setUsagePackage(usagePackage, getHtmlElementName(htmlElement), eventName);
     });
 }
 
-function setUsagePackage(usagePackage, name, eventName) {
-    if(usagePackage[name]){
-        usagePackage[name].push({event: eventName, timestamp: Date.now()});
+function setUsagePackage(usagePackage, Elementname, eventName) {
+    if(usagePackage[Elementname]){
+        usagePackage[Elementname].push({event: eventName, timestamp: Date.now()});
     } else {
-        usagePackage[name] = [];
-        usagePackage[name].push({event: eventName, timestamp: Date.now()});
+        usagePackage[Elementname] = [];
+        usagePackage[Elementname].push({event: eventName, timestamp: Date.now()});
     }
 
     console.log(usagePackage);
@@ -38,3 +41,33 @@ function setUsagePackage(usagePackage, name, eventName) {
 function getHtmlElementName(htmlElement) {
     return htmlElement.attributes.formcontrolname.textContent;
 }
+
+function addExtraOperationsForListeners(eventName, event, htmlElement){
+    switch(eventName) {
+        case "keyup":
+            handleKeyUpEvent(event, htmlElement);
+          break;
+        default:
+          // code block
+      }
+}
+
+function handleKeyUpEvent(event, htmlElement) {
+
+    if(htmlElement.value.length == 0) {
+        setUsagePackage(usagePackage, getHtmlElementName(htmlElement), "Empty")
+    } 
+
+
+    if(event.key === "Backspace") {
+        console.log(htmlElement.value);
+        setUsagePackage(usagePackage, getHtmlElementName(htmlElement), "Backspace")
+    }
+    else if(event.key === "Delete") {
+        setUsagePackage(usagePackage, getHtmlElementName(htmlElement), "Delete")
+    }
+    else {
+        setUsagePackage(usagePackage, getHtmlElementName(htmlElement), "Typing")
+    }
+}
+
