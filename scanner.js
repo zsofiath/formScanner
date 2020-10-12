5723bde2975fd5939c44009edea63c3d04029729
Tóth Zsófia <zsofiath@gmail.com>
2020-10-12 11:58:59 +0200

    package is created

--- scanner.js
+++ scanner.js
@@ -1,6 +1,9 @@
-var usagePackage = [];
 var formEventNameList = ["click", "blur", "change", "keyup"];
 var USAGE_PACKAGE;
+
+function debug_showPackage() {
+    console.log(USAGE_PACKAGE);
+}
 
 let UsagePackage = function () {
    this.userName = "";
@@ -32,14 +35,29 @@ EventPackage.prototype.setElementName = function (elementName) {
     this.elementName = elementName;
     return this;
 };
+/**
+ * Sets the events position related to the whole monitor screen
+ * @param {*} x 
+ * @param {*} y 
+ */
 EventPackage.prototype.setScreenPosition = function (x, y) {
     this.screenPosition = {x: x, y: y};
     return this;
 };
+/**
+ * Sets the events position related to the current element on witch the event happened
+ * @param {*} x 
+ * @param {*} y 
+ */
 EventPackage.prototype.setElementPosition = function (x, y) {
     this.elementPosition = {x: x, y: y};
     return this;
 };
+/**
+ * Sets the events position related to the document
+ * @param {*} x 
+ * @param {*} y 
+ */
 EventPackage.prototype.setDocumentPosition = function (x, y) {
     this.documentPosition = {x: x, y: y};
     return this;
@@ -58,54 +76,66 @@ EventPackage.prototype.setDocumentHeight = function (height) {
 
 // Content loaded
 document.addEventListener("DOMContentLoaded", function(){
-
-    USAGE_PACKAGE = new UsagePackage();
-   
+    USAGE_PACKAGE = new UsagePackage();  
     eddEventListenersToEveryFormElement(collectFormElements());
  });
 
 // Host softvare in focus
 window.onfocus = function () { 
-  console.log("____onfocus"); 
+    USAGE_PACKAGE
+    .AddEventPackage()
+    .setEvent("onfocus")
+    .setElementName("document")
+    .setDocumentWidth(window.width)
+    .setDocumentHeight(window.height);
 }; 
 
-// Host softvare in background
+// Host software in background
 window.onblur = function () { 
-  console.log("____onblur");
+    USAGE_PACKAGE
+    .AddEventPackage()
+    .setEvent("onblur")
+    .setElementName("document")
+    .setDocumentWidth(window.width)
+    .setDocumentHeight(window.height);
 }; 
 
 // mouse move
 document.onmousemove = function(event){
-   //console.log(event);
-   //console.log(event.metaKey);
-   var htmlElement = event.target;
-   var positionInTargetElementX = event.offsetX;
-   var positionInTargetElementY = event.offsetY;
-   var documentX = event.pageX;
-   var documentY = event.pageY;
-   event.screenX;
-   event.screenY;
-   // egér rángatás (ha > 50 akkor elég idegesen rángatja az egeret)
-   event.movementX
-   event.movementY
+
+   USAGE_PACKAGE
+   .AddEventPackage()
+   .setEvent("onmousemove")
+   .setElementName(getHtmlElementId(event.target))
+   .setElementPosition(event.offsetX, event.offsetY)
+   .setDocumentPosition(event.pageX, event.pageY)
+   .setScreenPosition(event.screenX, event.screenY)
+   .setDocumentWidth(window.width)
+   .setDocumentHeight(window.height);
+
 }
 
 // host softvare was clicked
 document.onclick = function(event){
-   // console.log(event);
+    USAGE_PACKAGE
+    .AddEventPackage()
+    .setEvent("onclick")
+    .setElementName(getHtmlElementId(event.target));
 }
-
 
 // the page is visible, or not
 document.addEventListener("visibilitychange", function(event) {
 
-    console.log(document.visibilityState);
-    console.log(event);
-
     if (document.visibilityState === 'visible') {
-        console.log('foreground')
+        USAGE_PACKAGE
+        .AddEventPackage()
+        .setEvent("visible")
+        .setElementName("document");
     } else {
-        console.log('idle')
+        USAGE_PACKAGE
+        .AddEventPackage()
+        .setEvent("idle")
+        .setElementName("document");
     }
   });
 
@@ -132,30 +162,22 @@ function eddEventListenersToEveryFormElement(elements){
 function setListeners(eventName, htmlElement){
     htmlElement.addEventListener(eventName, function(event) {
         addExtraOperationsForListeners(eventName, event, htmlElement);
-        setUsagePackage(usagePackage, getHtmlElementName(htmlElement), eventName);
+        setFormUsagePackage(USAGE_PACKAGE, getHtmlElementName(htmlElement), eventName);
     });
 }
 
-function setUsagePackage(usagePackage, Elementname, eventName) {
-    usagePackage.push({ElementName: Elementname, event: eventName, timestamp: Date.now()});
-    USAGE_PACKAGE
+function setFormUsagePackage(usagePackage, Elementname, eventName) {
+    usagePackage
     .AddEventPackage()
-    .setEvent("click")
-    .setElementName("hui")
-    .setScreenPosition(10,10)
-    .setElementPosition(2,0)
-    .setDocumentPosition(12,13)
-    .setDocumentWidth(200)
-    .setDocumentHeight(100);
-
-
-    console.log("--------------------------------------");
-    console.log(USAGE_PACKAGE);
-    console.log("--------------------------------------");
+    .setEvent(eventName)
+    .setElementName(Elementname);
 }
 
 function getHtmlElementName(htmlElement) {
     return htmlElement.attributes.formcontrolname.textContent;
+}
+function getHtmlElementId(htmlElement) {
+    return "TODO: get sme id";
 }
 
 function addExtraOperationsForListeners(eventName, event, htmlElement){
@@ -173,7 +195,6 @@ function handleKeyUpEvent(event, htmlElement) {
     if(htmlElement.value.length == 0) {
         setUsagePackage(usagePackage, getHtmlElementName(htmlElement), "Empty")
     } 
-
 
     if(event.key === "Backspace") {
         console.log(htmlElement.value);
