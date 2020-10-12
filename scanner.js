0dcf939953d9f6cef310fedf2ade0de8e50f2570
Tóth Zsófia <zsofiath@gmail.com>
2020-10-12 11:00:39 +0200

    create types

--- scanner.js
+++ scanner.js
@@ -1,34 +1,96 @@
 var usagePackage = [];
 var formEventNameList = ["click", "blur", "change", "keyup"];
+var USAGE_PACKAGE;
+
+let UsagePackage = function () {
+   this.userName = "";
+   this.taskId = '';
+   this.taskType = '';
+   this.eventList = []; //EventPackage[]
+};
+
+let EventPackage = function () {
+   this.elementName = "";
+   this.eventName = "";
+   this.elementPosition = {x: null, y: null};
+   this.screenPosition = {x: null, y: null};
+   this.documentPosition = {x: null, y: null};
+   this.documentSize = {width: null, heigth: null};
+   this.dateTime = new Date();
+};
+
+UsagePackage.prototype.AddEventPackage = function () {
+    let eventPackage = new EventPackage();
+    this.eventList.push(eventPackage);
+    return eventPackage;
+};
+EventPackage.prototype.setEvent = function (eventName) {
+    this.eventName = eventName;
+    return this;
+};
+EventPackage.prototype.setElementName = function (elementName) {
+    this.elementName = elementName;
+    return this;
+};
+EventPackage.prototype.setScreenPosition = function (x, y) {
+    this.screenPosition = {x: x, y: y};
+    return this;
+};
+EventPackage.prototype.setElementPosition = function (x, y) {
+    this.elementPosition = {x: x, y: y};
+    return this;
+};
+EventPackage.prototype.setDocumentPosition = function (x, y) {
+    this.documentPosition = {x: x, y: y};
+    return this;
+};
+EventPackage.prototype.setDocumentWidth = function (width) {
+    this.documentSize.width = width;
+    return this;
+};
+EventPackage.prototype.setDocumentHeight = function (height) {
+    this.documentSize.height = height;
+    return this;
+};
 
 
-  window.onfocus = function () { 
-    console.log("_________onfocus"); 
-  }; 
-  
-  window.onblur = function () { 
-    console.log("____onblur");
-  }; 
+// ============================================= Events =============================================
 
- // mouse move
- document.onmousemove = function(event){
-    //console.log(event);
-    //console.log(event.metaKey);
-    var htmlElement = event.target;
-    var positionInTargetElementX = event.offsetX;
-    var positionInTargetElementY = event.offsetY;
+// Content loaded
+document.addEventListener("DOMContentLoaded", function(){
 
-    var documentX = event.pageX;
-    var documentY = event.pageY;
+    USAGE_PACKAGE = new UsagePackage();
+   
+    eddEventListenersToEveryFormElement(collectFormElements());
+ });
 
-    event.screenX;
-    event.screenY;
+// Host softvare in focus
+window.onfocus = function () { 
+  console.log("____onfocus"); 
+}; 
 
-    // egér rángatás (ha > 50 akkor elég idegesen rángatja az egeret)
-    event.movementX
-    event.movementY
+// Host softvare in background
+window.onblur = function () { 
+  console.log("____onblur");
+}; 
+
+// mouse move
+document.onmousemove = function(event){
+   //console.log(event);
+   //console.log(event.metaKey);
+   var htmlElement = event.target;
+   var positionInTargetElementX = event.offsetX;
+   var positionInTargetElementY = event.offsetY;
+   var documentX = event.pageX;
+   var documentY = event.pageY;
+   event.screenX;
+   event.screenY;
+   // egér rángatás (ha > 50 akkor elég idegesen rángatja az egeret)
+   event.movementX
+   event.movementY
 }
 
+// host softvare was clicked
 document.onclick = function(event){
    // console.log(event);
 }
@@ -47,28 +109,17 @@ document.addEventListener("visibilitychange", function(event) {
     }
   });
 
+// ====================================================== functions ============================================================
 
-
-
-// event listeners on form elements
-document.addEventListener("DOMContentLoaded", function(){
-
-
-   
-
-
-
-
-
+function collectFormElements() {
     input =  Array.prototype.slice.call(document.getElementsByTagName("input"), 0);
     select =  Array.prototype.slice.call(document.getElementsByTagName("select"), 0);
     textarea =  Array.prototype.slice.call(document.getElementsByTagName("textarea"), 0);
     button =  Array.prototype.slice.call(document.getElementsByTagName("button"), 0);
 
-   var elements = [].concat(input).concat(select).concat(textarea).concat(button)
-   
-   eddEventListenersToEveryFormElement(elements);
-});
+   var elements = [].concat(input).concat(select).concat(textarea).concat(button);
+   return elements;
+}
 
 function eddEventListenersToEveryFormElement(elements){
     elements.forEach(htmlElement => {
@@ -87,6 +138,20 @@ function setListeners(eventName, htmlElement){
 
 function setUsagePackage(usagePackage, Elementname, eventName) {
     usagePackage.push({ElementName: Elementname, event: eventName, timestamp: Date.now()});
+    USAGE_PACKAGE
+    .AddEventPackage()
+    .setEvent("click")
+    .setElementName("hui")
+    .setScreenPosition(10,10)
+    .setElementPosition(2,0)
+    .setDocumentPosition(12,13)
+    .setDocumentWidth(200)
+    .setDocumentHeight(100);
+
+
+    console.log("--------------------------------------");
+    console.log(USAGE_PACKAGE);
+    console.log("--------------------------------------");
 }
 
 function getHtmlElementName(htmlElement) {
